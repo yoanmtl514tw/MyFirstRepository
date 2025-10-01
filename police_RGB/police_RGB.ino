3c4cfd167889452311d34ed873e2bd996845028d
yoanmtl514tw <t114012009@ntut.edu.tw>
2025-10-01 02:00:01 -0400

    Update police_RGB.ino

--- police_RGB/police_RGB.ino
+++ police_RGB/police_RGB.ino
@@ -1,27 +1,2 @@
-// the setup function runs once when you press reset or power the board
-//Constant definition
-
-const int rLEDPin = 3;
-const int gLEDPin = 4;
-const int bLEDPin = 5;
-
-void setup() {
-  // initialize digital pin 6 as an output.
-  pinMode(rLEDPin, OUTPUT);
-  pinMode(gLEDPin, OUTPUT);
-  pinMode(bLEDPin, OUTPUT);
-
-}
-
-// the loop function runs over and over again forever
-void loop() {
-  digitalWrite(rLEDPin, LOW); //RED LED ON
-  digitalWrite(gLEDPin, HIGH);
-  digitalWrite(bLEDPin, HIGH);
-  delay(200); 
-
-  digitalWrite(rLEDPin, HIGH);
-  digitalWrite(gLEDPin, HIGH);
-  digitalWrite(bLEDPin, LOW);  //BLUE LED ON
-  delay(200);
+const int pushButton =2;
 
